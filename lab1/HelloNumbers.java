2cebd913c78ae96a2a14fbc3f2aa48f7e6ca9e4f
wei <2640010800@qq.com>
2022-09-04 12:31:41 +0800

    complete the lab2

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,10 +1,10 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
-	int s = 0;
+	    int s = 0;
         while (x < 10) {
-	    s = s + x;
-	    x = x + 1;
+            s = s + x;
+            x = x + 1;
 	    System.out.print(s + " ");
         }
     }
